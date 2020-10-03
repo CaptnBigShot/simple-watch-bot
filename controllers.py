6ef600144589a50ef015757d3e2da301dfedbf0e
captain <danieljf@protonmail.com>
2020-10-03 11:04:54 -0400

    Cleaned up controller methods and made them more thread-safe.

--- controllers.py
+++ controllers.py
@@ -46,16 +46,151 @@ class MailController(object):
             mail_server.quit()
 
 
-class WatchlistController(object):
-    def __init__(self, watchlist: Watchlist):
-        self.watchlist = watchlist
+class WatchlistItemController(object):
+    def __init__(self, driver: webdriver, watchlist_item: WatchlistItem):
+        self.driver = driver
+        self.watchlist_item = watchlist_item
+        self.watchlist_item_web_element = None
 
-    @staticmethod
-    def create_alert(watchlist_item: WatchlistItem, alert_message: str):
-        title = "SimpleWatchBot: " + watchlist_item.name + " " + str(datetime.now())
-        message = "An alert condition has been met for '" + watchlist_item.name + "': " + alert_message + "\n"
-        message += "URL: " + watchlist_item.url
+    def __go_to_page(self):
+        self.driver.get(self.watchlist_item.url)
+
+    def __create_alert(self, alert_message: str):
+        title = "SimpleWatchBot: " + self.watchlist_item.name + " " + str(datetime.now())
+        message = "An alert condition has been met for '" + self.watchlist_item.name + "': " + alert_message + "\n"
+        message += "URL: " + self.watchlist_item.url
         return WatchlistItemAlert(title, message)
+
+    def __perform_select_option_by_text_step(self, step: WatchlistItemPreconditionStep):
+        element = self.driver.find_element(step.element_selector_type, step.element_selector)
+        select = Select(element)
+        select.select_by_visible_text(step.details["option_text"])
+
+    def __perform_click_element_step(self, step: WatchlistItemPreconditionStep):
+        element = self.driver.find_element(step.element_selector_type, step.element_selector)
+        element.click()
+
+    def __perform_precondition_step(self, step: WatchlistItemPreconditionStep):
+        if step.action == "select_option_by_text":
+            self.__perform_select_option_by_text_step(step)
+        elif step.action == "click_element":
+            self.__perform_click_element_step(step)
+
+    def __perform_precondition_steps(self):
+        for step in self.watchlist_item.precondition_steps:
+            self.__perform_precondition_step(step)
+
+    def __find_watchlist_item_element(self):
+        try:
+            return self.driver.find_element(self.watchlist_item.element_selector_type,
+                                            self.watchlist_item.element_selector)
+        except NoSuchElementException:
+            return None
+
+    def __check_element_condition_is_displayed(self):
+        return self.watchlist_item_web_element is not None \
+                and self.watchlist_item.alert_condition.is_displayed
+
+    def __check_element_condition_is_not_displayed(self):
+        return self.watchlist_item_web_element is None \
+                and self.watchlist_item.alert_condition.is_not_displayed
+
+    def __check_element_condition_text_equals(self):
+        return self.watchlist_item.alert_condition.text_equals is not None \
+                and self.watchlist_item_web_element.text == self.watchlist_item.alert_condition.text_equals
+
+    def __check_element_condition_text_not_equals(self):
+        return self.watchlist_item.alert_condition.text_not_equals is not None \
+                and self.watchlist_item_web_element.text != self.watchlist_item.alert_condition.text_not_equals
+
+    def __check_element_condition_text_contains(self):
+        return self.watchlist_item.alert_condition.text_contains is not None \
+                and self.watchlist_item.alert_condition.text_contains in self.watchlist_item_web_element.text
+
+    def __check_element_condition_text_not_contains(self):
+        return self.watchlist_item.alert_condition.text_not_contains is not None \
+                and self.watchlist_item.alert_condition.text_not_contains not in self.watchlist_item_web_element.text
+
+    def __check_watchlist_item_element_conditions(self):
+        self.watchlist_item_web_element = self.__find_watchlist_item_element()
+        if self.watchlist_item_web_element is None:
+            if self.__check_element_condition_is_not_displayed():
+                return "Element is not displayed."
+            elif self.watchlist_item.alert_condition.is_displayed:
+                pass
+            else:
+                raise Exception("Error: expected element to be displayed, but could not be found.")
+        else:
+            if self.__check_element_condition_is_displayed():
+                return "Element is displayed."
+            elif self.__check_element_condition_text_equals():
+                return "Text equals " + self.watchlist_item.alert_condition.text_equals
+            elif self.__check_element_condition_text_not_equals():
+                return "Text doesn't equal " + self.watchlist_item.alert_condition.text_not_equals
+            elif self.__check_element_condition_text_contains():
+                return "Text contains " + self.watchlist_item.alert_condition.text_contains
+            elif self.__check_element_condition_text_not_contains():
+                return "Text doesn't contain " + self.watchlist_item.alert_condition.text_not_contains
+        return None
+
+    def check_watchlist_item(self):
+        alert = None
+        try:
+            logging.info('Started checking item: ' + self.watchlist_item.name)
+            self.__go_to_page()
+            if self.watchlist_item.precondition_steps is not None:
+                self.__perform_precondition_steps()
+            alert_message = self.__check_watchlist_item_element_conditions()
+            history = WatchlistItemCheckHistory(alert_message, False, str(datetime.now()))
+            self.watchlist_item.check_history.append(history)
+            logging.info('Item ' + self.watchlist_item.name + ' alert message: ' + (
+                alert_message if alert_message is not None else 'N/A'))
+            if alert_message is not None and not self.watchlist_item.alert_condition.has_condition_been_met:
+                alert = self.__create_alert(alert_message)
+            self.watchlist_item.alert_condition.has_condition_been_met = alert is not None
+        except Exception as e:
+            history = WatchlistItemCheckHistory(str(e), True, str(datetime.now()))
+            self.watchlist_item.check_history.append(history)
+            if len(self.watchlist_item.check_history) >= 5 \
+                    and all(item.did_error for item in self.watchlist_item.check_history[-5:]):
+                alert = self.__create_alert(str(e))
+                self.watchlist_item.is_active = False
+            logging.error(e)
+        return alert
+
+
+class WatchlistController(object):
+    def __init__(self, watchlist: Watchlist, mail_controller: MailController, webdriver_settings: WebdriverSettings):
+        self.watchlist = watchlist
+        self.mail_controller = mail_controller
+        self.webdriver_settings = webdriver_settings
+        self.max_num_of_workers = webdriver_settings.max_number_of_workers
+
+    def check_watchlist_item(self, watchlist_item: WatchlistItem):
+        driver_controller = WebdriverController(self.webdriver_settings)
+        driver_controller.create_driver()
+        try:
+            watchlist_item_controller = WatchlistItemController(driver_controller.driver, watchlist_item)
+            watchlist_item_alert = watchlist_item_controller.check_watchlist_item()
+            if watchlist_item_alert is not None:
+                self.mail_controller.send_email(watchlist_item_alert.title, watchlist_item_alert.message)
+        finally:
+            driver_controller.close_driver()
+
+    def check_watchlist_items_concurrently(self):
+        logging.info('Started checking watchlist items')
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_num_of_workers) as executor:
+            future_watchlist_item = {executor.submit(self.check_watchlist_item, item): item
+                                     for item in self.watchlist.items if item.is_active}
+            for future in concurrent.futures.as_completed(future_watchlist_item):
+                watchlist_item = future_watchlist_item[future]
+                try:
+                    future.result()
+                except Exception as exc:
+                    logging.info(('%s generated an exception: %s' % (watchlist_item.name, exc)))
+                else:
+                    logging.info('Done checking item ' + watchlist_item.name)
+        logging.info('Done checking watchlist items')
 
 
 class WebdriverController(object):
@@ -72,139 +207,22 @@ class WebdriverController(object):
     def close_driver(self):
         self.driver.quit()
 
-    def go_to_page(self, url: str):
-        self.driver.get(url)
-
-    def perform_select_option_by_text_step(self, step: WatchlistItemPreconditionStep):
-        element = self.driver.find_element(step.element_selector_type, step.element_selector)
-        select = Select(element)
-        select.select_by_visible_text(step.details["option_text"])
-
-    def perform_click_element_step(self, step: WatchlistItemPreconditionStep):
-        element = self.driver.find_element(step.element_selector_type, step.element_selector)
-        element.click()
-
-    def perform_precondition_step(self, step: WatchlistItemPreconditionStep):
-        if step.action == "select_option_by_text":
-            self.perform_select_option_by_text_step(step)
-        elif step.action == "click_element":
-            self.perform_click_element_step(step)
-
-    def perform_precondition_steps(self, watchlist_item: WatchlistItem):
-        for step in watchlist_item.precondition_steps:
-            self.perform_precondition_step(step)
-
-    def find_watchlist_item_element(self, watchlist_item: WatchlistItem):
-        try:
-            return self.driver.find_element(watchlist_item.element_selector_type, watchlist_item.element_selector)
-        except NoSuchElementException:
-            return None
-
-    def check_watchlist_item_element_conditions(self, watchlist_item: WatchlistItem):
-        watchlist_item_element = self.find_watchlist_item_element(watchlist_item)
-
-        # is not displayed
-        if watchlist_item_element is None:
-            if watchlist_item.alert_condition.is_not_displayed:
-                return "Element is not displayed."
-            elif watchlist_item.alert_condition.is_displayed:
-                pass
-            else:
-                raise Exception("Error: expected element to be displayed, but could not be found.")
-
-        # is displayed
-        if watchlist_item_element is not None \
-                and watchlist_item.alert_condition.is_displayed:
-            return "Element is displayed."
-
-        # text equals
-        if watchlist_item.alert_condition.text_equals is not None \
-                and watchlist_item_element.text == watchlist_item.alert_condition.text_equals:
-            return "Text equals " + watchlist_item.alert_condition.text_equals
-
-        # text not equals
-        if watchlist_item.alert_condition.text_not_equals is not None \
-                and watchlist_item_element.text != watchlist_item.alert_condition.text_not_equals:
-            return "Text doesn't equal " + watchlist_item.alert_condition.text_not_equals
-
-        # text contains
-        if watchlist_item.alert_condition.text_contains is not None \
-                and watchlist_item.alert_condition.text_contains in watchlist_item_element.text:
-            return "Text contains " + watchlist_item.alert_condition.text_contains
-
-        # text not contains
-        if watchlist_item.alert_condition.text_not_contains is not None \
-                and watchlist_item.alert_condition.text_not_contains not in watchlist_item_element.text:
-            return "Text doesn't contain " + watchlist_item.alert_condition.text_not_contains
-
-        return None
-
 
 class MainController(object):
     def __init__(self):
         self.data_controller = DataController()
         self.deserialized_data_file = self.data_controller.read_data_file()
-        self.watchlist_controller = WatchlistController(self.deserialized_data_file.watchlist)
         self.mail_controller = MailController(self.deserialized_data_file.mail_settings)
         self.webdriver_settings = self.deserialized_data_file.webdriver_settings
-        self.max_number_of_workers = self.webdriver_settings.max_number_of_workers
-
-    def check_watchlist_item(self, watchlist_item: WatchlistItem):
-        driver_controller = WebdriverController(self.webdriver_settings)
-        driver_controller.create_driver()
-        try:
-            logging.info('Started checking item: ' + watchlist_item.name)
-            driver_controller.go_to_page(watchlist_item.url)
-            if watchlist_item.precondition_steps is not None:
-                driver_controller.perform_precondition_steps(watchlist_item)
-            alert_message = driver_controller.check_watchlist_item_element_conditions(watchlist_item)
-            history = WatchlistItemCheckHistory(alert_message, False, str(datetime.now()))
-            watchlist_item.check_history.append(history)
-            logging.info('Item ' + watchlist_item.name + ' alert message: ' + (
-                alert_message if alert_message is not None else 'N/A'))
-            if alert_message is not None and not watchlist_item.alert_condition.has_condition_been_met:
-                alert = self.watchlist_controller.create_alert(watchlist_item, alert_message)
-                self.mail_controller.send_email(alert.title, alert.message)
-            watchlist_item.alert_condition.has_condition_been_met = alert_message is not None
-        except Exception as e:
-            history = WatchlistItemCheckHistory(str(e), True, str(datetime.now()))
-            watchlist_item.check_history.append(history)
-            if len(watchlist_item.check_history) > 2 \
-                    and all(item.did_error for item in watchlist_item.check_history[-3:]):
-                alert = self.watchlist_controller.create_alert(watchlist_item, str(e))
-                self.mail_controller.send_email(alert.title, alert.message)
-                watchlist_item.is_active = False
-            logging.error(e)
-        finally:
-            driver_controller.close_driver()
-
-    def check_watchlist_items(self):
-        logging.info('Started checking watchlist items..')
-        for watchlist_item in self.watchlist_controller.watchlist.items:
-            self.check_watchlist_item(watchlist_item)
-
-    def check_watchlist_items_concurrently(self):
-        logging.info('Started checking watchlist items')
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_number_of_workers) as executor:
-            future_watchlist_item = {executor.submit(self.check_watchlist_item, item): item
-                                     for item in self.watchlist_controller.watchlist.items if item.is_active}
-            for future in concurrent.futures.as_completed(future_watchlist_item):
-                item = future_watchlist_item[future]
-                try:
-                    future.result()
-                except Exception as exc:
-                    logging.info(('%s generated an exception: %s' % (item.name, exc)))
-                else:
-                    logging.info('Done checking item ' + item.name)
-        # data = json.dumps(self.deserialized_data_file, default=lambda o: o.__dict__, sort_keys=False, indent=2)
-        # self.data_controller.write_data_file(data)
-        logging.info('Done checking watchlist items')
+        self.watchlist_controller = WatchlistController(self.deserialized_data_file.watchlist,
+                                                        self.mail_controller, self.webdriver_settings)
+        logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
 
     def check_watchlist_items_with_recheck(self):
         try:
             while True:
                 start_time = time.time()
-                self.check_watchlist_items_concurrently()
+                self.watchlist_controller.check_watchlist_items_concurrently()
                 end_time = time.time()
                 elapsed_time = end_time - start_time
                 sleep_time = max(self.watchlist_controller.watchlist.recheck_num_of_seconds - elapsed_time, 0)
@@ -212,6 +230,7 @@ class MainController(object):
                     logging.info('Re-running in ' + str(round(sleep_time, 2)) + ' seconds')
                     time.sleep(sleep_time)
         finally:
-            pass
+            # write updated data to file
             # data = json.dumps(self.deserialized_data_file, default=lambda o: o.__dict__, sort_keys=False, indent=2)
             # self.data_controller.write_data_file(data)
+            pass
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import logging
-
 from controllers import MainController
 
 
@@ -8,5 +6,4 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
     main()
